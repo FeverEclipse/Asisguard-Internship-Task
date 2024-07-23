92e7e0230c0dca0865477e39520edd287da99330
Eyüp Menevşe <menevseeyup@gmail.com>
2024-07-23 13:46:42 +0300

    improved connection

--- eleHandler.py
+++ eleHandler.py
@@ -1,5 +1,6 @@
 import requests
 import socket
+import select
 
 UDP_IP = "10.10.10.54"
 UDP_LATLON_PORT = 11453
@@ -11,20 +12,29 @@ lon = 0
 def elevation():
     global lat
     global lon
-    url = "https://api.open-elevation.com/api/v1/lookup?locations=" + str(lat) + "," + str(lon)
+    if lat == "":
+        lat = 0
+    if lon == "":
+        lon = 0
+    url = f"https://api.open-elevation.com/api/v1/lookup?locations={lat},{lon}"
     r = requests.get(url=url)
     data = r.json()
-    elevation = data['results'][0]['elevation']
-    return elevation
+    return data['results'][0]['elevation']
+
+# Create and bind the socket once
+latlon_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+latlon_socket.bind((UDP_IP, UDP_LATLON_PORT))
+
+# Create the socket for sending elevation data
+elevation_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((UDP_IP, UDP_LATLON_PORT))
-    msg, address = s.recvfrom(1024)
-    print("Received Message!")
-    msg = msg.decode("utf-8")
-    lat = msg.split(",")[0]
-    lon = msg.split(",")[0]
-    ele = elevation()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(str(ele).encode("utf-8"), (UDP_IP, UDP_ELE_PORT))
+    # Use select to wait for data with a timeout
+    ready_sockets, _, _ = select.select([latlon_socket], [], [], 1)
+    if ready_sockets:
+        msg, address = latlon_socket.recvfrom(1024)
+        print("Received Message!")
+        msg = msg.decode("utf-8")
+        lat, lon = msg.split(",")
+        ele = elevation()
+        elevation_socket.sendto(str(ele).encode("utf-8"), (UDP_IP, UDP_ELE_PORT))
--- elevation.py
+++ elevation.py
@@ -1,17 +1,7 @@
 import time
-import requests
 import tkinter
 import threading
 import socket
-
-
-
-global stop_flag
-global stop_receive
-stop_flag = threading.Event()
-stop_receive = threading.Event()
-task_thread = None
-receive_thread = None
 
 UDP_IP = "10.10.10.54"
 UDP_LATLON_PORT = 11453
@@ -20,15 +10,15 @@ UDP_ELE_PORT = 1453
 latitude = "0"
 longitude = "0"
 
+stop_flag = threading.Event()
+stop_receive = threading.Event()
+task_thread = None
+receive_thread = None
+
 gui = tkinter.Tk()
 
 def startRetrieve():
-    global stop_flag
-    global task_thread
-    global latitudeEntry
-    global longitudeEntry
-    global latitude
-    global longitude
+    global stop_flag, task_thread, receive_thread, latitudeEntry, longitudeEntry
     latitude = latitudeEntry.get()
     longitude = longitudeEntry.get()
     stop_flag.clear()
@@ -38,17 +28,12 @@ def startRetrieve():
     receive_thread.start()
 
 def sendLatLon():
-    global stop_flag
-    global latitude
-    global longitude
-    global latitudeEntry
-    global longitudeEntry
+    global stop_flag, latitudeEntry, longitudeEntry
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while not stop_flag.is_set():
         latitude = latitudeEntry.get()
         longitude = longitudeEntry.get()
-        message = str(latitude) + "," + str(longitude)
-        message = message.encode("utf-8")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        message = f"{latitude},{longitude}".encode("utf-8")
         sock.sendto(message, (UDP_IP, UDP_LATLON_PORT))
         time.sleep(1)
 
@@ -56,19 +41,10 @@ def receiveElevation():
     global eleLabel
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((UDP_IP, UDP_ELE_PORT))
-    while True:
-        msg, address = s.recvfrom(1024)
+    while not stop_flag.is_set():
+        msg, _ = s.recvfrom(1024)
         print(msg.decode("utf-8"))
         eleLabel.config(text="The requested elevation is: " + msg.decode("utf-8"))
-
-'''def elevation():
-    global eleLabel
-    url = "https://api.open-elevation.com/api/v1/lookup?locations=" + str(latitude) + "," + str(longitude)
-    r = requests.get(url=url)
-    data = r.json()
-    elevation = data['results'][0]['elevation']
-    print(data)
-    eleLabel.config(text=("The requested elevation is: " + str(elevation)))'''
 
 latitudeLabel = tkinter.Label(gui, text='Enter latitude: ')
 latitudeLabel.grid(row=0,column=0, sticky="W", padx=15, pady=15)
