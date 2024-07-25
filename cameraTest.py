eb4a6289dbd472c38ed793ce81a7c9b1d7bca8d9
Gokhan PACAL <gokhan.pacal@asisguard.com.tr>
2024-07-25 14:50:52 +0300

    dummy commit.

--- cameraTest.py
+++ cameraTest.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 ser = serial.Serial(
-    port='COM9',
+    port='COM26',
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
@@ -16,10 +16,6 @@ initbytes = b"\x81\x01\x04\x19\x01\xFF"
 defaultzoombytes = b"\x81\x01\x04\x47\x00\x00\x00\x00\xFF"
 fullzoombytes = b"\x81\x01\x04\x47\x04\x00\x00\x00\xFF"
 
-ser.write(initbytes)
-time.sleep(0.5)
-ser.write(defaultzoombytes)
-time.sleep(1)
 curTime = time.time()
 ser.write(fullzoombytes)
 time.sleep(0.05) # Wait a little to guarentee that a response has received
@@ -27,5 +23,12 @@ ReceivedData = b""
 while ser.inWaiting() > 0:
     ReceivedData += ser.read(1) # Read bytes from the device until finished
 # decoded = ''.join(f'{byte:02X}' for byte in ReceivedData) # Decode the received bytecode to string
-if ReceivedData == b"\x90\x42\xFF\x90\x52\xFF":
-    print("elapsed: " , time.time() - curTime - 0.05)
+received2 = b""
+count = 0
+if ReceivedData == b"\x90\x42\xFF":
+    while ser.in_waiting() > 0:
+        received2 += ser.read(1)
+    if received2 == b"\x90\x52\xFF":
+        print("elapsed: " , time.time() - curTime - 0.05)
+else:
+    print(received2.hex())
