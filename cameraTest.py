1aae3699ac014045febe075aa79399e4042405d4
Gokhan PACAL <gokhan.pacal@asisguard.com.tr>
2024-07-25 15:17:53 +0300

    Shorter calculation.

--- cameraTest.py
+++ cameraTest.py
@@ -16,22 +16,11 @@ initbytes = b"\x81\x01\x04\x19\x01\xFF"
 defaultzoombytes = b"\x81\x01\x04\x47\x00\x00\x00\x00\xFF"
 fullzoombytes = b"\x81\x01\x04\x47\x04\x00\x00\x00\xFF"
 
-curTime = time.time()
 ser.write(fullzoombytes)
-time.sleep(0.05) # Wait a little to guarentee that a response has received
+curTime = time.time()
 ReceivedData = b""
-while ser.inWaiting() > 0:
-    ReceivedData += ser.read(1) # Read bytes from the device until finished
-# decoded = ''.join(f'{byte:02X}' for byte in ReceivedData) # Decode the received bytecode to string
-received2 = b""
-count = 0
-if ReceivedData == b"\x90\x42\xFF":
-    while ser.in_waiting() == 0:
-        time.sleep(0.05)
-        count += 1
-    while ser.in_waiting() > 0:
-        received2 += ser.read(1)
-    if received2 == b"\x90\x52\xFF":
-        print("elapsed: " , time.time() - curTime - (0.05 * count) - 0.05)
-else:
-    print(received2.hex())
+while ser.inWaiting() < 6:
+    time.sleep(0.01) # Read bytes from the device until finished
+ReceivedData += ser.read(6)
+if ReceivedData == b"\x90\x42\xFF\x90\x52\xFF":
+    print("elapsed: " , time.time() - curTime)
